743a7f2607b781086f30aefa32ee2ad682120e0a
Whited Williams <61710729+M6HqVBcddw2qaN4s@users.noreply.github.com>
2022-05-15 03:16:17 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+	const video = document.getElementById("video");
+	const start = document.getElementById("start");
+	const stop = document.getElementById("stop");
+
+	var displayMediaOptions = {
+		video: {
+			cursor: "always",
+		},
+		audio: true,
+	};
+
+	start.onclick = function (e) {
+		startSharing();
+	};
+
+	stop.onclick = function (e) {
+		stopSharing();
+	};
+
+	async function startSharing() {
+		let capture = null;
+		try {
+			capture = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+
+			capture
+				.getVideoTracks()[0]
+				.addEventListener("ended", () => stopSharing());
+
+			video.srcObject = capture;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	function stopSharing() {
+		let tracks = video.srcObject.getTracks();
+		tracks.forEach((track) => track.stop());
+		video.srcObject = null;
+	}
